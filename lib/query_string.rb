d9868c606726783da13a64d2ecff594f4ac7eef1
Alexander Popov <alex.wayfer@gmail.com>
2018-07-06 16:53:10 +0300

    Change code (variables naming) to Rack-style

--- lib/query_string.rb
+++ lib/query_string.rb
@@ -4,23 +4,17 @@ require 'cgi'
 # Module with class methods for convertation
 module QueryString
   class << self
-    def build(value, key = nil)
+    def build(value, prefix = nil)
       case value
       when Hash
-        value.map { |k, v| build(v, append_key(key, k)) }.join('&')
+        value.map { |k, v| build(v, prefix ? "#{prefix}[#{k}]" : k) }.join('&')
       when Array
-        value.map { |v| build(v, "#{key}[]") }.join('&')
+        value.map { |v| build(v, "#{prefix}[]") }.join('&')
       when nil
         ''
       else
-        "#{key}=#{CGI.escape(value.to_s)}"
+        "#{prefix}=#{CGI.escape(value.to_s)}"
       end
-    end
-
-    private
-
-    def append_key(root_key, key)
-      root_key.nil? ? key : "#{root_key}[#{key}]"
     end
   end
 end
